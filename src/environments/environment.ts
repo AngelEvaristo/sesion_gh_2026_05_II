8cf0ac5b5cb1434b131bbb34a6573b474232d55b
AngelEvaristo <62860453+AngelEvaristo@users.noreply.github.com>
2026-05-21 21:42:59 -0500

    push sin secreto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000',
-  githubToken: "ErSyL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mdy1M2ELcQSFoS8CW1nkZXyp0KBaeN"
+  githubToken: ""
 };
